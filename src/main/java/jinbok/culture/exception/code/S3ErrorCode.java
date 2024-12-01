886fce1f298ae78944b9a26ffcb2d339c2e41dda
jjinbok <haba8730@naver.com>
2024-12-01 23:19:58 +0900

    feature : S3 에러 코드 추가

--- src/main/java/jinbok/culture/exception/code/S3ErrorCode.java
+++ src/main/java/jinbok/culture/exception/code/S3ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum S3ErrorCode implements ErrorCode {
 
-    INVALID_IMAGE(HttpStatus.BAD_REQUEST,"이미지가 존재하지 않습니다.");
+    INVALID_IMAGE(HttpStatus.BAD_REQUEST,"이미지가 존재하지 않습니다."),
+    FAILED_CONVERT_FILE(HttpStatus.BAD_REQUEST, "MultipartFile -> File 전환에 실패했습니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
