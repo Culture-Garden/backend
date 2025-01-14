25ad7f5bffa8c2be1d6c41c42af065041a808bb7
이진복 <126658197+jjinbok@users.noreply.github.com>
2025-01-14 21:12:36 +0900

    Update start-server.sh
    change port number because my server port number is 8088

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,6 @@
 
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/instagram-server
-sudo fuser -k -n tcp 8080 || true
+sudo fuser -k -n tcp 8088 || true
 nohup java -jar project.jar > ./output.log 2>&1 &
 echo "--------------- 서버 배포 끝 -----------------"
